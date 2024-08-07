532c7feb83e37c2b4ff5901021e2ece04d6c8844
JarrarShahid <148614865+JarrarShahid@users.noreply.github.com>
2024-08-07 21:04:00 +0500

    Add files via upload

---
+++ eleven.js
@@ -0,0 +1,92 @@
+// HackerLand University has the following grading policy:
+
+// Every student receives a grade  in the inclusive range from 0 to 100 .
+// Any grades less than 40 is a failing grade.
+// Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+// If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+// If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    return grades.map(grade => {
+        if (grade < 38) {
+            return grade;
+        }
+        let nextMultipleOf5 = Math.ceil(grade / 5) * 5;
+        if (nextMultipleOf5 - grade < 3) {
+            return nextMultipleOf5;
+        } else {
+            return grade;
+        }
+    });
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
